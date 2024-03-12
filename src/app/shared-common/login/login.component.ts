0265f4d3b9a81527292385d029e0b86829cc1793
Harsh Patel <harsh3608@gmail.com>
2024-03-12 18:06:54 +0530

    ...

--- src/app/shared-common/login/login.component.ts
+++ src/app/shared-common/login/login.component.ts
@@ -1,29 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit{
-  loginForm!:FormGroup;
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
   constructor(
-private fb:FormBuilder
-  ) {}
+    private fb: FormBuilder,
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: new FormControl(),
-      password: new FormControl(),
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(15),
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{6,15}')
+        ]),
     })
   }
 
 
 
   onSubmit() {
+    this.loginForm.markAllAsTouched();
+    debugger;
+    if (this.loginForm.valid) {
 
+    }else{
+debugger;
+    }
   }
 
 }
